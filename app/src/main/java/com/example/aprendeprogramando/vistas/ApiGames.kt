32973ef7c17da89322f4ae8133cddaba25c2f9eb
Emiliano Del Arco <emiliano.delarco@educacion.local>
2024-05-16 09:30:51 -0300

    clase 7 navegacion

---
+++ app/src/main/java/com/example/aprendeprogramando/vistas/ApiGames.kt
@@ -0,0 +1,13 @@
+package com.example.aprendeprogramando.vistas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ApiGames() {
+    Column {
+        Text("Juegos")
+        // Agregar código para mostrar imagenes de juegos
+    }
+}
---
+++ app/src/main/java/com/example/aprendeprogramando/vistas/TaskListScreen.kt
@@ -0,0 +1,13 @@
+package com.example.aprendeprogramando.vistas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskListScreen() {
+    Column {
+        Text("Lista de tareas")
+        // Agregar código para mostrar y administrar tareas
+    }
+}
